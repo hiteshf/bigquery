f80b4c7ba2315f30fedb0f62199bcd724f8c71f9
hfuletra <hiteshptl82@gmail.com>
2022-10-21 16:36:57 -0700

    update SQL and Readme

--- README.md
+++ README.md
@@ -3,4 +3,7 @@ Ref : All these SQLs are built upon Bigquery INFORMATION_SCHEMA.To learn more vi
 Contains SQLs I use regurlary to check bigquery job stats.
 
 - sql\count_by_user.sql : Shows information about number of queries for each user under the project along with time of longest query executed in last 24hrs 
-- sql\count_by_job_status.sql : Show similar information as above query however this one gives stats by job status. This is really useful to check and find out if there are very high number of queries running or pending during any hour of the day.
+- sql\count_by_job_status.sql : Show similar information as above query however this one gives stats by job status. 
+    - This query gives you how many pending/running/pending jobs hourly
+    - with state='RUNNING' where clause, it shows time of longest running query in minutes along with higest slots consumed slots by any query so far.
+    - with state='PENDING' where claues, it shows pending queries with time of longest waiting query in minutes.
--- sql/count_by_job_status.sql
+++ sql/count_by_job_status.sql
@@ -1,16 +1,44 @@
-with vw as (SELECT
-  user_email,job_id,
-  query,
-  job.state,
-  DATETIME(TIMESTAMP_TRUNC(job.creation_time, HOUR), "America/Los_Angeles") AS creation_time,
-  max(round(TIMESTAMP_DIFF(ifnull(end_time,current_timestamp()),ifnull(start_time,creation_time),SECOND)/60,3)) over (partition by user_email) as max_query_execution_time_min,
-  round(TIMESTAMP_DIFF(ifnull(end_time,current_timestamp()),ifnull(start_time,creation_time),SECOND)/60,3) query_execution_time_min,
-  count(query) over (partition by user_email) query_count_by_user_in_24hr
+WITH
+  vw AS (
+  SELECT
+    job_id,
+    query,
+    job.state,
+    job.total_slot_ms/(1000 * 60 * 60) slots_consumed_by_query,
+    EXTRACT(HOUR
+    FROM
+      DATETIME(job.creation_time, "America/Los_Angeles")) AS run_hour,
+    EXTRACT(DATE
+    FROM
+      DATETIME(job.creation_time, "America/Los_Angeles")) AS run_date,
+    MAX(job.total_slot_ms/(1000 * 60 * 60)) OVER (PARTITION BY state) higest_slots_consumed_by_query,
+    MAX(ROUND(TIMESTAMP_DIFF(IFNULL(end_time,CURRENT_TIMESTAMP()),IFNULL(start_time,creation_time),SECOND)/60,3) ) OVER (PARTITION BY state) AS max_query_execution_time_min,
+    ROUND(TIMESTAMP_DIFF(IFNULL(end_time,CURRENT_TIMESTAMP()),IFNULL(start_time,creation_time),SECOND)/60,3) query_execution_time_min,
+    COUNT(state) OVER (PARTITION BY state) count_by_query_state
+  FROM
+    `region-us`.INFORMATION_SCHEMA.JOBS job
+  WHERE
+    creation_time BETWEEN TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 6 hour )
+    AND CURRENT_TIMESTAMP()
+    AND job_type = 'QUERY' )
+SELECT
+  state,
+  count_by_query_state,
+  max_query_execution_time_min,
+  higest_slots_consumed_by_query,
+  ARRAY(
+  SELECT
+    AS STRUCT job_id,query,run_hour,run_date,query_execution_time_min,slots_consumed_by_query
+  FROM
+    vw v2
+  WHERE
+    v2.state=v1.state ) AS queries
 FROM
-  `region-us`.INFORMATION_SCHEMA.JOBS job
+  vw v1
 WHERE
-  creation_time BETWEEN TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 1 DAY )
-  AND CURRENT_TIMESTAMP()
-  )
-  select  user_email,query_count_by_user_in_24hr,max_query_execution_time_min,  array(select as struct   query,creation_time,state,query_execution_time_min    from vw v2 where v2.user_email=v1.user_email ) as queries   from vw v1  
- group by 1,2,3
+  state='RUNNING'
+GROUP BY
+  1,
+  2,
+  3,
+  4
